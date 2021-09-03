ab1625fdc77d57aed20ad01dfcb3b80cdcc2e2ba
Taushik Ahmed Joy <taushikjoy@gmail.com>
2021-09-03 16:36:30 +0600

    fixed carusol funcionality

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ closeButton.addEventListener('click' , ()=> {
 let curSlide = 0;
 let maxSlide=slides.length;
 
+
+// console.log(maxSlide);
+
 rightButton.addEventListener('click' , function(){
 
   if (curSlide === maxSlide-1) {
@@ -37,14 +40,52 @@ rightButton.addEventListener('click' , function(){
 }
 );
 
+let backCurslide = slides.length;
+let minSlide = 0;
+
 backButton.addEventListener('click' , function(){
 
-  if (curSlide === maxSlide-1) {
-    curSlide=0;
+  if (backCurslide === 0) {
+    backCurslide= slides.length-1 ;
+
   }
   else{
-  curSlide--; }
+    backCurslide--;
+  }
+ 
+  
   slides.forEach((s , i ) => 
-   ( s.style.transform = `translateX(${100*(i-curSlide)}%)` ) )
+   ( s.style.transform = `translateX(${(100*(i-backCurslide))}%)` ) )
+  //  console.log(i);
 }
 );
+
+// OPERATION TAB //
+let cont1 = document.querySelector('.operation-content-1');
+let cont2 = document.querySelector('.operation-content-2');
+let cont3 = document.querySelector('.operation-content-3');
+
+let tab3 = document.querySelector('.operation-tab-3');
+let tab2 = document.querySelector('.operation-tab-2');
+let tab1 = document.querySelector('.operation-tab-1');
+
+tab3.addEventListener('click' , function(){
+
+  cont1.classList.remove('operations-content-active')
+  cont2.classList.remove('operations-content-active')
+  cont3.classList.add('operations-content-active')
+})
+
+tab2.addEventListener('click' , function(){
+
+  cont1.classList.remove('operations-content-active')
+  cont2.classList.add('operations-content-active')
+  cont3.classList.remove('operations-content-active')
+})
+
+tab1.addEventListener('click' , function(){
+
+  cont2.classList.remove('operations-content-active')
+  cont3.classList.remove('operations-content-active')
+  cont1.classList.add('operations-content-active')
+})
